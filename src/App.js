1964b3dd5a5799070dbe8879a35c4e06045104ce
yan zhang <circusburger63@yahoo.com>
2024-05-06 16:18:55 -0700

    styling

--- src/App.js
+++ src/App.js
@@ -5,20 +5,34 @@ function App() {
   const [input, setInput] = useState("");
   const [res, setRes] = useState([]);
 
+  const url = "http://www.omdbapi.com/?i=tt3896198&apikey=9fe1e7a9";
+
   useEffect(() => {
-    const url = `https://jsonplaceholder.typicode.com/posts`;
-    fetch(url)
-      .then((response) => response.json())
-      .then((json) => setRes(json))
-      .catch((error) => console.error(error));
+    if (input.length > 3) {
+      fetch(url + `&s=${input}`)
+        .then((response) => response.json())
+        .then((json) => {
+          const { Search } = json;
+          if (Search) {
+            setRes(Search);
+          }
+        })
+        .catch((error) => console.error(error));
+    }
   }, [input]);
 
   return (
     <div className="App">
       <input name="location" onChange={(e) => setInput(e.target.value)} />
-      {res.map((ele) => (
-        <div>{ele.id}</div>
-      ))}
+      <section class="results">
+        {res.map((r, idx) => {
+          return (
+            <div class="result-item" key={idx}>
+              {r.Title}
+            </div>
+          );
+        })}
+      </section>
     </div>
   );
 }
